452f07a9dbcc2a5f3216ac62b44cebb29d4ca2da
rafaelmaub <deepcavestudios@gmail.com>
2024-04-22 14:11:42 -0400

    Pop Ups system and Asset Clean Up

---
+++ Assets/Mahjong/Scripts/ScriptableObjects/PopUpData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PopUpData : ScriptableObject
+{
+    public string Title => title;
+    public string Description => description;
+    public PopUpType PopUpType => questionType;
+
+    [SerializeField] private string title;
+    [SerializeField] private string description;
+    [SerializeField] private PopUpType questionType;
+
+
+
+}
+
+public enum PopUpType
+{
+    YesNo, Confirm
+}
+
